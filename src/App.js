61033f44a606c0588ab75e91d8572f1e19c1c504
Demitra Del Fiacco <ddelfiacco99@gmail.com>
2020-01-22 21:45:02 -0600

    authorization finished

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import CardGrid from './Components/CardGrid';
 import SidebarWrapper from './Components/SideBarWrapper';
 import { ShoppingContext } from './context';
-import { Grid, Button, Label } from 'semantic-ui-react';
+import { Grid, Button, Label, Header } from 'semantic-ui-react';
+import firebase from 'firebase/app';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
 const App = () => {
   const shoppingContext = useContext(ShoppingContext);
   const { setVisible, cartTotal } = shoppingContext;
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(setUser);
+  }, []);
 
   const Content = () => {
     return(
       <Grid padded='vertically'>
         <Grid.Row>
           <Grid.Column width={15}>
-            <div />
+            <Banner user={user} />
           </Grid.Column>
           <Grid.Column width={1}>
             <Button 
@@ -32,8 +40,37 @@ const App = () => {
   };
 
   return (
-    <SidebarWrapper content={<Content />} />
+    <SidebarWrapper content={<Content />} /> 
   );
 };
+
+const uiConfig = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ],
+  callbacks: {
+    signInSuccessWithAuthResult: () => false
+  }
+};
+
+const SignIn = () => (
+  <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+);
+
+const Banner = ({ user }) => (
+  !user ? <SignIn /> :
+  <Header as="h4">
+    {!user ? 'Welcome,  Shopper' : `Welcome, ${user.displayName}`}
+    <Button 
+      style={{ marginLeft: "15px" }} 
+      compact
+      color="yellow"
+      content="Log Out" 
+      onClick={() => firebase.auth().signOut()} 
+    />
+  </Header>
+);
+
 
 export default App;
