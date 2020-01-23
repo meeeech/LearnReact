966fffc6074358ce659b92775d71e6cd5cc35299
Demitra Del Fiacco <ddelfiacco99@gmail.com>
2020-01-22 22:37:41 -0600

    fixed render bug

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import CardGrid from './Components/CardGrid';
 import SidebarWrapper from './Components/SideBarWrapper';
 import { ShoppingContext } from './context';
 import { Grid, Button, Label, Header } from 'semantic-ui-react';
-import firebase from 'firebase/app';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
 const App = () => {
   const shoppingContext = useContext(ShoppingContext);
   const { setVisible, cartTotal } = shoppingContext;
 
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged(setUser);
-  }, []);
-
   const Content = () => {
     return(
       <Grid padded='vertically'>
         <Grid.Row>
-          <Grid.Column width={15}>
-            <Banner user={user} />
-          </Grid.Column>
-          <Grid.Column width={1}>
-            <Button 
-              color='yellow'
-              label={<Label content={cartTotal.items} />}
-              icon="cart" 
-              onClick={() => setVisible(true)} 
-            />
+          <Grid.Column width={13}/>
+          <Grid.Column width={3} >
+            <Button.Group onClick={() => setVisible(true)}> 
+              <Button icon='user' attached='left' basic color="yellow" />
+              <Button 
+                attached='right'
+                color='yellow'
+                label={<Label content={cartTotal.items} />}
+                icon="cart"  
+              />
+            </Button.Group>
           </Grid.Column>
         </Grid.Row> 
         <Grid.Row>
@@ -43,34 +36,6 @@ const App = () => {
     <SidebarWrapper content={<Content />} /> 
   );
 };
-
-const uiConfig = {
-  signInFlow: 'popup',
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID
-  ],
-  callbacks: {
-    signInSuccessWithAuthResult: () => false
-  }
-};
-
-const SignIn = () => (
-  <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
-);
-
-const Banner = ({ user }) => (
-  !user ? <SignIn /> :
-  <Header>
-    {!user ? 'Welcome,  Shopper' : `Welcome, ${user.displayName}`}
-    <Button 
-      style={{ marginLeft: "15px" }} 
-      compact
-      color="yellow"
-      content="Log Out" 
-      onClick={() => firebase.auth().signOut()} 
-    />
-  </Header>
-);
 
 
 export default App;
--- src/Components/SideBarWrapper.js
+++ src/Components/SideBarWrapper.js
@@ -1,12 +1,20 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { Menu, Sidebar, Header, Icon, Button, Container, Divider } from 'semantic-ui-react';
 import { ShoppingContext } from '../context';
 import MenuCard from './MenuCard';
+import firebase from 'firebase/app';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
 const SidebarWrapper = ({ content }) => {
   const shoppingContext = useContext(ShoppingContext);
   const { visible, setVisible, cartItems, cartTotal } = shoppingContext;
   const [refresh, setRefresh] = useState(false);
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(setUser);
+  }, []);
 
   return (
     <Sidebar.Pushable>
@@ -21,6 +29,8 @@ const SidebarWrapper = ({ content }) => {
       >
         <Button inverted icon='close' onClick={() => setVisible(false)} />
         <Menu.Item>
+          <Banner user={user}/>
+          <Divider inverted />
           <Header inverted textAlign='center'>
             <Icon name='cart' inverted />CART
           </Header>
@@ -43,5 +53,33 @@ const SidebarWrapper = ({ content }) => {
     </Sidebar.Pushable>
   );
 };
+
+const uiConfig = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ],
+  callbacks: {
+    signInSuccessWithAuthResult: () => false
+  }
+};
+
+const SignIn = () => (
+  <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+);
+
+const Banner = ({ user }) => (
+  !user ? <SignIn /> :
+  <Header inverted>
+    {!user ? 'Welcome,  Shopper' : `Welcome, ${user.displayName}`}
+    <Button 
+      floated='right'
+      compact
+      color="yellow"
+      content="Log Out" 
+      onClick={() => firebase.auth().signOut()} 
+    />
+  </Header>
+);
 
 export default SidebarWrapper;
